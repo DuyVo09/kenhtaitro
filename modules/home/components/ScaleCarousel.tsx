91ccdce33ca0b00a7a72ba6e818a1d954ebf488e
duyvo <duy.vo@aivision.vn>
2023-12-04 18:34:03 +0700

    update

--- modules/home/components/ScaleCarousel.tsx
+++ modules/home/components/ScaleCarousel.tsx
@@ -14,6 +14,7 @@ export function ScaleCarousel() {
   const sliderSetting: Settings = {
     // className: "center",
     dots: true,
+    variableWidth: true,
     // centerMode: true,
     infinite: true,
     centerPadding: "10px",
