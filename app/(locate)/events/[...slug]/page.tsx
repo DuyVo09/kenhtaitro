83275e56949f0093dd1fd41f01e737c726247e9d
duyvo <duy.vo@aivision.vn>
2023-12-30 22:57:19 +0700

    add slug

--- app/(locate)/events/[...slug]/page.tsx
+++ app/(locate)/events/[...slug]/page.tsx
@@ -3,28 +3,28 @@ import { Box } from "@mui/material";
 
 async function fetchSlugsFromIYourAPI() {
   // Replace this with your own logic to fetch the slugs from your API
-  const slugs = await fetch('/api/slugs')
+  const slugs = await fetch("/api/slugs")
     .then((response) => response.json())
     .catch((error) => {
-      console.error('Error fetching slugs:', error);
+      console.error("Error fetching slugs:", error);
       return ["mock-slug-1", "mock-slug-2"];
     });
 
   return slugs;
 }
 
-export async function getStaticPaths() {
+export async function generateStaticParams() {
   // Replace this with your own logic to fetch the possible values for [...slug]
   const slugs = await fetchSlugsFromIYourAPI();
 
   const paths = slugs.map((slug: string) => ({
-    params: { slug: slug.split('/') }, // Split the slug into its parts
+    params: { slug: slug.split("/") }, // Split the slug into its parts
   }));
 
   return { paths, fallback: false };
-} 
+}
 
-function EventDetailPage() {
+function EventDetailPage({ params }: { params: { slug: string } }) {
   return (
     <Box className="flex-col flex w-full">
       <OverviewSection />
