40c737e80496681666a50f1a2c105f32aab85f49
duyvo <duy.vo@aivision.vn>
2023-12-30 23:13:06 +0700

    update slug get func

--- app/(locate)/events/[...slug]/page.tsx
+++ app/(locate)/events/[...slug]/page.tsx
@@ -1,27 +1,14 @@
 import { DetailSection, OverviewSection, RelatedSection } from "./_sections";
 import { Box } from "@mui/material";
 
-async function fetchSlugsFromIYourAPI() {
-  // Replace this with your own logic to fetch the slugs from your API
-  const slugs = await fetch("/api/slugs")
-    .then((response) => response.json())
-    .catch((error) => {
-      console.error("Error fetching slugs:", error);
-      return ["mock-slug-1", "mock-slug-2"];
-    });
-
-  return slugs;
-}
-
 export async function generateStaticParams() {
   // Replace this with your own logic to fetch the possible values for [...slug]
   const slugs = ["mock-slug-1", "mock-slug-2"];
 
-  const paths = slugs.map((slug: string) => ({
+  return slugs.map((slug: string) => ({
     params: { slug: slug.split("/") }, // Split the slug into its parts
   }));
 
-  return { paths, fallback: false };
 }
 
 function EventDetailPage({ params }: { params: { slug: string } }) {
